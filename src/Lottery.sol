0d96a6bb1d32d480677add3d906a48cd3c9bf3b8
dirrong <jihyun.yoo@dream-academy.io>
2022-10-01 21:37:03 +0900

    test

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,1 +1,25 @@
-dnj
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Lottery {
+
+
+    constructor () {
+
+    }
+
+    function buy() {
+
+    }
+
+    function claim() {
+
+    }
+
+    function draw() {
+
+    }
+
+
+
+}
